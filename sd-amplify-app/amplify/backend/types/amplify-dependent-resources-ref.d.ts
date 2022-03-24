bdc02585fefe77724d969661a630738bc525f2a5
EC2 Default User <ec2-user@ip-172-31-34-215.ap-southeast-1.compute.internal>
2022-03-24 06:37:58 +0000

    update readme

--- sd-amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ sd-amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,23 +6,5 @@ export type AmplifyDependentResourcesAttributes = {
             "Region": "string",
             "LambdaExecutionRole": "string"
         }
-    },
-    "api": {
-        "SmartDocApi": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    },
-    "auth": {
-        "swiftsmartdoc133567d5": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
     }
 }
